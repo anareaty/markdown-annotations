6160751437432438f1d726d3fa8ace480a2ceda1
Anareaty <reatyrabota@yandex.ru>
2023-09-27 20:20:50 +0300

    Bug fix

--- main.ts
+++ main.ts
@@ -18,11 +18,11 @@ export default class MyPlugin extends Plugin {
 
 
 		const openSideNotes = async() => {
-			let currentFile = app.workspace.activeEditor.file
+			let currentFile = this.app.workspace.activeEditor.file
 			let currentFolder = currentFile.parent.path
 			let currentName = currentFile.basename
 
-			let properties = app.workspace.activeEditor.metadataEditor.properties
+			let properties = this.app.workspace.activeEditor.metadataEditor.properties
 			let sideNotesProp = properties.find(p => p.key == "Примечания") 
 			let sideNotesLink = ""
 
@@ -32,28 +32,28 @@ export default class MyPlugin extends Plugin {
 			} else {
 
 				sideNotesLink = currentFolder + "/" + currentName + " — примечания"
-				await app.fileManager.processFrontMatter(currentFile, (frontmatter) => { 
+				await this.app.fileManager.processFrontMatter(currentFile, (frontmatter) => { 
 						frontmatter["Примечания"] = sideNotesLink
 					})
 			}
 			
-			let sideNotesFile = app.vault.getAbstractFileByPath(sideNotesLink + ".md")
+			let sideNotesFile = this.app.vault.getAbstractFileByPath(sideNotesLink + ".md")
 			if (!sideNotesFile) {
-				sideNotesFile = await app.vault.create(currentFolder + "/" + currentName + " — примечания" + ".md", "")
+				sideNotesFile = await this.app.vault.create(currentFolder + "/" + currentName + " — примечания" + ".md", "")
 			}
 
-			let sideLeaf = await app.workspace.createLeafBySplit(app.workspace.activeLeaf, "vertical")
+			let sideLeaf = await this.app.workspace.createLeafBySplit(this.app.workspace.getActiveViewOfType(MarkdownView), "vertical")
 			await sideLeaf.openFile(sideNotesFile, { focus: false } );
 		}
 
 
 
 		const saveQuoteToSideNotes = async () => {
-			let currentFile = app.workspace.activeEditor.file
+			let currentFile = this.app.workspace.activeEditor.file
 			let currentFolder = currentFile.parent.path
 			let currentName = currentFile.basename
 
-			let selection = await app.workspace.activeEditor.getSelection()
+			let selection = await this.app.workspace.activeEditor.getSelection()
 			let selectionFirstLine = selection.split("\n")[0]
 			let shortenedFirstLine = selectionFirstLine.slice(0, 40)
 			if (shortenedFirstLine.length < selectionFirstLine.length) {
@@ -64,7 +64,7 @@ export default class MyPlugin extends Plugin {
 			selectionHighlight = selectionHighlight.replaceAll("====", "")
 
 
-			let properties = app.workspace.activeEditor.metadataEditor.properties
+			let properties = this.app.workspace.activeEditor.metadataEditor.properties
 			let sideNotesProp = properties.find(p => p.key == "Примечания") 
 			let sideNotesLink = ""
 
@@ -74,17 +74,17 @@ export default class MyPlugin extends Plugin {
 			} else {
 
 				sideNotesLink = currentFolder + "/" + currentName + " — примечания"
-				await app.fileManager.processFrontMatter(currentFile, (frontmatter) => { 
+				await this.app.fileManager.processFrontMatter(currentFile, (frontmatter) => { 
 						frontmatter["Примечания"] = sideNotesLink
 					})
 			}
 			
-			let sideNotesFile = app.vault.getAbstractFileByPath(sideNotesLink + ".md")
+			let sideNotesFile = this.app.vault.getAbstractFileByPath(sideNotesLink + ".md")
 			if (!sideNotesFile) {
-				sideNotesFile = await app.vault.create(currentFolder + "/" + currentName + " — примечания" + ".md", "")
+				sideNotesFile = await this.app.vault.create(currentFolder + "/" + currentName + " — примечания" + ".md", "")
 			}
 
-			let sideNotesContent = await app.vault.read(sideNotesFile)
+			let sideNotesContent = await this.app.vault.read(sideNotesFile)
 
 			const checkHeader = (header, headerNum) => {
 				let re = new RegExp(header)
@@ -99,10 +99,10 @@ export default class MyPlugin extends Plugin {
 			let header = "# " + shortenedFirstLine
 			header = checkHeader(header, 1)
 
-			await app.vault.modify(sideNotesFile, sideNotesContent + "\n\n##" + header + "\n" + selectionQuote)
+			await this.app.vault.modify(sideNotesFile, sideNotesContent + "\n\n##" + header + "\n" + selectionQuote)
 			let insertLink = "[[" + sideNotesLink + header + "|📝]]"
 
-			await app.workspace.activeEditor.editor.replaceSelection(selectionHighlight + " " + insertLink)
+			await this.app.workspace.activeEditor.editor.replaceSelection(selectionHighlight + " " + insertLink)
 
 		}
 
